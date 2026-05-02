7872bbc572587ed6713374c66921ef9dd5a31872
Sofía Modernell <sofia.modernell@estudiantes.utec.edu.uy>
2026-05-02 17:24:34 -0300

    feat: Update image paths and Vite base URL
    Updates image paths within the project to use a relative structure and configures the Vite base URL to '/Portafolio_Sofia_Modernell/' for proper deployment on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './',
+    base: '/Portafolio_Sofia_Modernell/',
     plugins: [
       react(),
       tailwindcss(),
